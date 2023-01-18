d4a8c527deb54fdc325477e6cf87c8fe896a68fe
julia.satsyk <julia.satsyk@gmail.com>
2023-01-18 20:45:43 +0200

    Added simple calculations

--- src/com/ua/robotdreams/Main.java
+++ src/com/ua/robotdreams/Main.java
@@ -2,6 +2,11 @@ package com.ua.robotdreams;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int num1 = 5;
+        int num2 = 8;
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Subtraction: " + (num1 - num2));
+        System.out.println("Division: " + (num1 / num2));
+        System.out.println("Multiplication: " + (num1 * num2));
     }
 }
