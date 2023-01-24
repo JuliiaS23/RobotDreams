d776e04efb57d910048b3a450a92f6062c0edb5b
julia.satsyk <julia.satsyk@gmail.com>
2023-01-24 19:50:33 +0200

    Added String operations showcase

--- src/com/ua/robotdreams/Main.java
+++ src/com/ua/robotdreams/Main.java
@@ -2,11 +2,10 @@ package com.ua.robotdreams;
 
 public class Main {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 8;
-        System.out.println("Addition: " + (num1 + num2));
-        System.out.println("Subtraction: " + (num1 - num2));
-        System.out.println("Division: " + (num1 / num2));
-        System.out.println("Multiplication: " + (num1 * num2));
+      String str = "developers";
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(str.length()-1));
     }
 }
