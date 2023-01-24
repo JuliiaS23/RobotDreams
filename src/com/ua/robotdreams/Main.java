511c7ee5b99f7c343a39d3cccbcf0c9863a0b950
julia.satsyk <julia.satsyk@gmail.com>
2023-01-24 20:08:47 +0200

    Added basic operands showcase

--- src/com/ua/robotdreams/Main.java
+++ src/com/ua/robotdreams/Main.java
@@ -4,9 +4,15 @@ public class Main {
     public static void main(String[] args) {
         int num1 = 5;
         int num2 = 8;
-        System.out.println("Addition: " + (num1 + num2));
-        System.out.println("Subtraction: " + (num1 - num2));
-        System.out.println("Division: " + (num1 / num2));
-        System.out.println("Multiplication: " + (num1 * num2));
+        if(num1 > num2) {
+            System.out.println(num1 + " is bigger than " + num2);
+        }
+        else if (num1 < num2) {
+            System.out.println(num1 + " is lesser than " + num2);
+        }
+        else {
+            System.out.println("Operands are equal");
+        }
+
     }
 }
