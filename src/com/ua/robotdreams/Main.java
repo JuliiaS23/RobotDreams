c39e913751d2f676cb760a1abd10baef54619768
julia.satsyk <julia.satsyk@gmail.com>
2023-02-06 20:23:35 +0200

    Completed set of tasks with methods

--- src/com/ua/robotdreams/Main.java
+++ src/com/ua/robotdreams/Main.java
@@ -1,40 +1,36 @@
 package com.ua.robotdreams;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=====TASK 1=====");
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(i + " ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number to bring to cube number: ");
+        int number = scanner.nextInt();
+        System.out.println(cubeNumber(number));
+        System.out.print("Enter the number of stars to print: ");
+        number = scanner.nextInt();
+        printStars(number);
+        System.out.print("Enter the number of symbols to print: ");
+        number = scanner.nextInt();
+        System.out.print("Enter the symbol to print: ");
+        String symbol = scanner.next();
+        printSymbolNumberTimes(number, symbol);
+    }
+
+    static int cubeNumber(int number) {
+        return number * number * number;
+    }
+    static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
         }
         System.out.println();
-
-        System.out.println("=====TASK 2=====");
-        for (int i = 0; i <= 100; i++) {
-            if (i % 2 == 0) {
-                System.out.print(i + " ");
-            }
+    }
+    static void printSymbolNumberTimes(int number, String symbol) {
+        for (int i = 0; i < number; i++) {
+            System.out.print(symbol);
         }
-        System.out.println();
-
-        System.out.println("=====TASK 3=====");
-        int factorial = 1;
-        for (int i = 1; i <= 7; i++) {
-            factorial *= i;
-        }
-        System.out.println(factorial);
-
-        System.out.println("=====TASK 4=====");
-        int n0 = 0;
-        int n1 = 1;
-        int n2;
-        System.out.print(n0 + " " + n1 + " ");
-        while (n0 + n1 < 100) {
-            n2 = n0 + n1;
-            System.out.print(n2 + " ");
-            n0 = n1;
-            n1 = n2;
-        }
-        System.out.println();
     }
 }
 
