d2acbbfdfeb8cfa6fcf03792ed3bd6f595210b08
julia.satsyk <julia.satsyk@gmail.com>
2023-02-09 11:36:38 +0200

    Completed set of tasks with arrays

--- src/com/ua/robotdreams/Main.java
+++ src/com/ua/robotdreams/Main.java
@@ -1,38 +1,80 @@
 package com.ua.robotdreams;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int[] array = new int[10];
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number to bring to cube number: ");
-        int number = scanner.nextInt();
-        System.out.println(cubeNumber(number));
-        System.out.print("Enter the number of stars to print: ");
-        number = scanner.nextInt();
-        printStars(number);
-        System.out.print("Enter the number of symbols to print: ");
-        number = scanner.nextInt();
-        System.out.print("Enter the symbol to print: ");
-        String symbol = scanner.next();
-        printSymbolNumberTimes(number, symbol);
-    }
-
-    static int cubeNumber(int number) {
-        return number * number * number;
-    }
-    static void printStars(int number) {
-        for (int i = 0; i < number; i++) {
-            System.out.print("*");
+        System.out.print("Enter max value of the range of the array: ");
+        int maxValue = scanner.nextInt();
+        arrayWithRandomNumbers(array, maxValue);
+        for (int j : array) {
+            System.out.print(j + " ");
         }
         System.out.println();
+        System.out.println("Min value is " + minValue(array));
+        System.out.println("Max value is " + maxValue(array));
+        System.out.println("Average value is " + averageValue(array));
+        System.out.println("Total value is " + totalValue(array));
     }
-    static void printSymbolNumberTimes(int number, String symbol) {
-        for (int i = 0; i < number; i++) {
-            System.out.print(symbol);
+
+    static void arrayWithRandomNumbers(int[] x, int maxValue) {
+        Random random = new Random();
+        for (int i = 0; i < x.length; i++) {
+            x[i] = random.nextInt(maxValue);
         }
     }
+
+    static void validateArray(int[] x) {
+        if (x == null) {
+            throw new NullPointerException("The array was null.");
+        }
+        if (x.length == 0) {
+            throw new IllegalArgumentException("The array had no items.");
+        }
+    }
+
+    static int minValue(int[] x) {
+        validateArray(x);
+        int currentMin = x[0];
+        for (int j : x) {
+            if (currentMin > j)
+                currentMin = j;
+        }
+        return currentMin;
+    }
+
+    static int maxValue(int[] x) {
+        validateArray(x);
+        int currentMax = x[0];
+        for (int j : x) {
+            if (currentMax < j)
+                currentMax = j;
+        }
+        return currentMax;
+    }
+
+    static int averageValue(int[] x) {
+        validateArray(x);
+        int total = 0;
+        for (int j : x) {
+            total += j;
+        }
+        return total / x.length;
+    }
+
+    static int totalValue(int[] x) {
+        validateArray(x);
+        int total = 0;
+        for (int j : x) {
+            total += j;
+        }
+        return total;
+    }
 }
+
 
 
 
